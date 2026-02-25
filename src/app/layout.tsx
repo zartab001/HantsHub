64a80d931bc40173568ad68970c4ab0d90b78604
zartab001 <zartab001@gmail.com>
2026-02-25 19:25:56 +0500

    feat: add Header component and integrate into layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/ui/Header";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -21,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${poppins.className} antialiased`}>
+        <Header />
         {children}
       </body>
     </html>
